de293e3668f8bf978c240279cb58f7afdc64f684
Vanderson <vanderson.lima@fusiondms.com.br>
2021-11-19 10:27:17 -0300

    Arquivo de execução

---
+++ exec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 
+# ---------------------------------------------------------------- #
+# Script Name:   exec.sh 
+# Description:   Script de execução
+# Site:          https://site.fusiondms.com.br/
+# Written by:    Vanderson Diniz
+# Maintenance:   FusionDMS
+# ---------------------------------------------------------------- #
+# Usage:         
+#       $ ./exe.sh ou sh exec.sh
+# -----------------------------------------------------------------# 
+# History:     
+# -> 19.11.2021 - vanderson.lima - criação do script
+# -----------------------------------------------------------------#
+
+#------INCLUDES--------->
+source ./script.sh
+#----------------------->
+
+funcao
