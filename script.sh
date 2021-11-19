7948344ba5b4ac982a4b3d4f95af3d491a6511ea
Vanderson <vanderson.lima@fusiondms.com.br>
2021-11-19 10:27:48 -0300

    Arquivo base para as funções dos demais scripts

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# ---------------------------------------------------------------- #
+# Script Name:   script.sh
+# Description:   Script para automatizar o processo de criação de
+#                branches, repositorios, entre outros
+# Site:          https://site.fusiondms.com.br/
+# Written by:    Vanderson Diniz
+# Maintenance:   FusionDMS
+# ---------------------------------------------------------------- #
+# Usage:
+#       $ ./script.sh ou sh script.sh
+# -----------------------------------------------------------------#
+# History:
+# -> 19.11.2021 - vanderson.lima - criação do script
+#
+# -----------------------------------------------------------------#
+
+#------VARS--------->
+
+#------------------->
+
+#------FUNCTIONS----------->
+function funcao() {
+    echo "HelloWorld!"
+}
+#-------------------------->
