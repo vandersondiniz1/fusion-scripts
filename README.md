451a1d929f4d05a99bf865ffa43feed34443fbdd
Vanderson <vanderson.lima@fusiondms.com.br>
2021-11-29 16:55:19 -0300

    [PSR]/[Pegando a última release do repositório]

---
+++ README.md
@@ -0,0 +1,3 @@
+//git for-each-ref --format '%(refname:short)' refs/heads # Local
+//git for-each-ref --format '%(refname:short)' refs/remotes # Remote
+//git describe --abbrev=0
--- src/core/app.php
+++ src/core/app.php
@@ -3,16 +3,31 @@
 require_once(__DIR__ . '\..\utils\Utils.php');
 
 $msg = true;
-$path = ("C:\Users\Fusion\Desktop\Fusion\Projetos\bitbucket/fusionweb");
+$path = ("C:\\Users\\Fusion\\Desktop\\Fusion\\Projetos\\bitbucket\\fusionweb");
+$get_release = "git branch -a --sort=-committerdate | grep release | sed -n '1 p'";
 
-// wellCome();
+logMsg('->Executing application ', 'info', 'api-server.php');
+logMsg("->cd 'C:\\Users\\Fusion\\Desktop\\Fusion\\Projetos\\bitbucket\\fusionweb' ", 'info', 'api-server.php');
 
-logMsg('->Executing application ', 'info', 'api-server.php', '\n');
+#ir no seu repositório local e executar um git fetch, pra atualizar as branchs
+#FIXME: precisamos definir qual sera a branch principal. Existe master, develop, prod2.5
+#acho que precisamos passar o nome da branch por parametro
+exec("cd $path && git checkout master");
+logMsg('->git checkout master', 'info', 'api-server.php');
 
-logMsg("->Updating repository ", "info", "api-server.php", "\n");
 exec("cd $path && git pull origin master && git fetch --all");
+logMsg('->git pull origin master && git fetch --all', 'info', 'api-server.php');
 
-logMsg('->Updating repository ', 'info', 'api-server.php', '\n');
-exec("cd $path && git describe --abbrev=0");
+exec("cd $path && git branch -a --sort=-committerdate | grep release | sed -n '1 p'", $output);
+$last_release = explode('remotes/origin/', $output[0]);
+logMsg("->git branch -a --sort=-committerdate | grep release | sed -n '1 p'", 'info', 'api-server.php');
+logMsg("->Last Release:$last_release[0] ", 'info', 'api-server.php');
 
+exec("cd $path && git checkout $last_release[0]");
+logMsg("->git checkout $last_release[0]", 'info', 'api-server.php');
 
+// exec("cd $path && git checkout -b Feature/ENG-M-I");
+// exec("cd $path && git checkout -b Feature/ENG-B-I");
+// exec("cd $path && git checkout -b Feature/ENG-H-I");
+
+logMsg('->Ending application ', 'info', 'api-server.php');
